14eeb67fc728d670a24a90cbb5541b780f5f8d93
Lasse Henriksen <lass5676@edu.easj.dk>
2019-02-01 11:36:21 +0100

    Tilføjet klassen demo og to properties

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.AccessControl;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class demo
+    {
+        private string _navn;
+        private int _alder;
+
+
+        public demo(string Navn, int Alder)
+        {
+            _navn = Navn;
+            _alder = Alder;
+            
+        }
+
+    }
+
+
+}
